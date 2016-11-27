5b55bee56642c58bdefd38cdb8e912f4435bd4d9
ThomasKenyeres <kenyerest97@gmail.com>
2016-11-27 22:49:17 +0100

    First part finished

--- printing.py
+++ printing.py
@@ -1,2 +1,11 @@
+from reports import *
 
 # Printing functions
+print("There are " + str(count_games("game_stat.txt")) + " in the file")
+print("It's " + str(decide("game_stat.txt", 1989)) + " that there's at least one game of the given year")
+print("The game from the latest year is " + str(get_latest("game_stat.txt")))
+print("There are " + str(count_by_genre("game_stat.txt", "First-person shooter")) + " of the given genre")
+print("The line number of the given name is: " + str(get_line_number_by_title("game_stat.txt", "Minecraft")))
+print("\nGames in alphabetical order: " + str(sort_abc("game_stat.txt")))
+print("\nGenres: " + str(get_genres("game_stat.txt")))
+print("Release date of top sold fps: " + str(when_was_top_sold_fps("game_stat.txt")))
--- reports.py
+++ reports.py
@@ -1,2 +1,99 @@
-
 # Report functions
+def count_games(file_name):
+    count = 0
+    file_ = open(file_name, "r")
+    for line in file_:
+        count += 1
+    file_.close()
+    return count
+
+
+def decide(file_name, year):
+    file_ = open(file_name, "r")
+    for line in file_:
+        record = line.strip().split("\t")
+        if int(record[2]) == year:
+            return True
+    return False
+
+
+def get_latest(file_name):
+    file_ = open(file_name, "r")
+    latest_year = 0
+    latest_title = ""
+    for line in file_:
+        record = line.strip().split("\t")
+        if int(record[2]) > latest_year:
+            latest_year = int(record[2])
+            latest_title = str(record[0])
+    return latest_title
+
+
+def count_by_genre(file_name, genre):
+    file_ = open(file_name, "r")
+    count = 0
+    for line in file_:
+        record = line.strip().split("\t")
+        if record[3] == genre:
+            count += 1
+    return count
+
+
+def get_line_number_by_title(file_name, title):
+    file_ = open(file_name, "r")
+    line_num = 0
+    for line in file_:
+        line_num += 1
+        record = line.strip().split("\t")
+        if record[0] == title:
+            return line_num
+    raise ValueError
+
+
+def sort_abc(file_name):
+    file_ = open(file_name, "r")
+    titles = []
+    titles_sorted = []
+    records = []
+    for line in file_:
+        record = line.strip().split("\t")
+        titles.append(record[0])
+
+    for j in range(len(titles)):
+        for i in range(len(titles)):
+            if titles[i] == min(titles):
+                titles_sorted.append(titles[i])
+                del titles[i]
+                break
+    return titles_sorted
+
+
+def get_genres(file_name): #SORT!
+    file_ = open(file_name, "r")
+    genres = []
+    genres_sorted = []
+    for line in file_:
+        record = line.strip().split("\t")
+        if not record[3] in genres:
+            genres.append(record[3])
+    for j in range(len(genres)):
+        for i in range(len(genres)):
+            if genres[i] == min(genres):
+                genres_sorted.append(genres[i])
+                del genres[i]
+                break
+    return genres_sorted
+
+
+def when_was_top_sold_fps(file_name):
+    file_ = open(file_name, "r")
+    top = 0
+    records = []
+    for line in file_:
+        record = line.strip().split("\t")
+        records.append(record)
+        if float(record[1]) > float(top) and (record[3] == "First-person shooter"):
+            top = record[1]
+    for record in records:
+        if record[1] == top:
+            return record[2]
